4452c03119c523fc989f8992fc8f4a73b29a76ff
HawraaYounes <hawraayounes0@gmail.com>
2023-02-26 16:49:22 +0200

    Video 18: Arrays

--- app.ts
+++ app.ts
@@ -6,3 +6,6 @@ const car1:{ name:string; mileage:number}={//here the properties 'name & mileage
 const car2:{ name:string; mileage?:number}={//here the property 'mileage' is not required (optional)
     name:"",
 };
+//Video 18: Arrays
+const names: readonly string[] = ["John"];//readonly keyword prevent arrays from being changed.
+// names.push("Jack");  //Error
