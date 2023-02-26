169ccf6be54546745a06928720c9d8c1f6bf60a0
HawraaYounes <hawraayounes0@gmail.com>
2023-02-26 17:14:30 +0200

    Video 18: Arrays

--- app.ts
+++ app.ts
@@ -6,6 +6,10 @@ const car1:{ name:string; mileage:number}={//here the properties 'name & mileage
 const car2:{ name:string; mileage?:number}={//here the property 'mileage' is not required (optional)
     name:"",
 };
+
 //Video 18: Arrays
 const names: readonly string[] = ["John"];//readonly keyword prevent arrays from being changed.
-// names.push("Jack");  //Error
+// names.push("Jack");  //Error Property 'push' does not exist on type 'readonly string[]'.
+let activities:any[]=[];// 'any' type can be used for mixed arrays
+
+
