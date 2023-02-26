c6fcfed590b0841edf96249ee95a3e1e033e23f1
HawraaYounes <hawraayounes0@gmail.com>
2023-02-26 15:08:00 +0200

    Video 16: Object Types

---
+++ app.ts
@@ -0,0 +1,8 @@
+//Video 16: Objects Types
+const car1:{ name:string; mileage:number}={//here the properties 'name & mileage' are both required
+    name:"Toyota",
+    mileage: 2000
+};
+const car2:{ name:string; mileage?:number}={//here the property 'mileage' is not required (optional)
+    name:"Toyota",
+};
