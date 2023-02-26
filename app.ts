51dea90a258ce675b11d465584f723b538a26362
HawraaYounes <hawraayounes0@gmail.com>
2023-02-26 15:17:44 +0200

    Removed node_modules folder

--- app.ts
+++ app.ts
@@ -1,7 +1,7 @@
 //Video 16: Objects Types
 const car1:{ name:string; mileage:number}={//here the properties 'name & mileage' are both required
     name:"Toyota",
-    mileage: 200
+    mileage: 3000
 };
 const car2:{ name:string; mileage?:number}={//here the property 'mileage' is not required (optional)
     name:"",
