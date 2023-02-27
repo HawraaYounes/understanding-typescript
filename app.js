f78cde835e1f0328d1263bdbddf1dc77b7954748
HawraaYounes <hawraayounes0@gmail.com>
2023-02-27 13:24:38 +0200

    Video 23: Literal Types

--- app.js
+++ app.js
@@ -7,3 +7,10 @@ function printStatusCode(code) {
 printStatusCode(404); //Correct (code is of type number)
 printStatusCode("404"); //Correct (code is of type string)
 //printStatusCode(true); //Error (code is of type boolean, only number or string is allowed)
+//Video 23: Literal Types
+function printText(s, alignment) {
+    console.log("'".concat(s, "' has alignment: ").concat(alignment));
+}
+printText("Hello world!", "left"); //Correct
+//printText("Hello world!", "Right"); //Error: values are CASE SENSITIVE ()
+//printText("Hello World!", "center"); //Error:Argument of type '"center"' is not assignable to parameter of type "left" | "right".
--- app.ts
+++ app.ts
@@ -1,4 +1,4 @@
-export{};
+export{}; //Telling TypeScript that our file is a module with its own scope.
 //Video 22: Union Types
     function printStatusCode(code: number | string){//code can be of type "number" OR "string"
         console.log(`My status cose is ${code}`);
@@ -6,3 +6,11 @@ export{};
     printStatusCode(404); //Correct (code is of type number)
     printStatusCode("404"); //Correct (code is of type string)
     //printStatusCode(true); //Error (code is of type boolean, only number or string is allowed)
+
+//Video 23: Literal Types
+function printText(s: string, alignment: "left" | "right" ) {//functions that accept only a certain set of values for alignment:
+    console.log(`'${s}' has alignment: ${alignment}`);
+  }
+  printText("Hello world!", "left"); //Correct
+  //printText("Hello world!", "Right"); //Error: values are CASE SENSITIVE ()
+  //printText("Hello World!", "center"); //Error:Argument of type '"center"' is not assignable to parameter of type "left" | "right".
