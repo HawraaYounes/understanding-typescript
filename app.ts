28ac315a1a123abd587b0be35f7fb0fd8bbe8926
HawraaYounes <hawraayounes0@gmail.com>
2023-02-27 14:13:43 +0200

    Video 24:Type Aliases / Custom Types

--- app.ts
+++ app.ts
@@ -1,4 +1,5 @@
 export{}; //Telling TypeScript that our file is a module with its own scope.
+
 //Video 22: Union Types
     function printStatusCode(code: number | string){//code can be of type "number" OR "string"
         console.log(`My status cose is ${code}`);
@@ -14,3 +15,14 @@ function printText(s: string, alignment: "left" | "right" ) {//functions that ac
   printText("Hello world!", "left"); //Correct
   //printText("Hello world!", "Right"); //Error: values are CASE SENSITIVE ()
   //printText("Hello World!", "center"); //Error:Argument of type '"center"' is not assignable to parameter of type "left" | "right".
+
+//Video 24: Type Aliases/ Custom Types
+  type id= number | string; //variables of type id can be either number or string.
+  type pet= 'Dog' | 'Cat'; //variables of type pet can be either 'Dog' or 'Cat'.
+  function combine(ID1: id, ID2: id, petName1: pet, petName2 : pet){
+    console.log(`'${petName1}' has ID: ${ID1}`);
+    console.log(`'${petName2}' has ID: ${ID2}`);
+  }
+  combine("#1902",2706,"Dog","Cat");//Correct
+  //combine(1859,"A1275","Fish","Dog");//Error: 'Fish' cannot be assigned to petName1 of type 'pet'(only Dog OR Cat is allowed)
+  //combine(5602,false,"Cat","Cat");//Error: false(boolean) cannot be assigned to ID2 of type 'id'(only string OR number is allowed)
