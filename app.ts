3878978c016d4a4bc31765640f9a552250732469
HawraaYounes <hawraayounes0@gmail.com>
2023-02-28 10:46:09 +0200

    Video 34: Using Watch Mode

---
+++ app.ts
@@ -0,0 +1,5 @@
+export{}; //Telling TypeScript that our file is a module with its own scope.
+
+//Video 34: Using "Watch Mode"
+    //Watch mode is used to recompile the file automatically without re-running "tsc app.ts" using the command:
+        // "tsc app.ts -w"  OR  "tsc app.ts --watch" 
