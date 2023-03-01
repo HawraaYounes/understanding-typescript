17769df964726effd4a22141e4f6d11514cf18a8
HawraaYounes <hawraayounes0@gmail.com>
2023-03-01 12:20:58 +0200

    Video 61:Constructor Functions & The this Keyword

--- dist/app.js
+++ dist/app.js
@@ -3,6 +3,9 @@ class Department {
     constructor(n) {
         this.name = n; //Assigning n to name property;
     }
+    describe() {
+        console.log("Department Name:" + this.name);
+    }
 }
 const accounting = new Department("Accounting"); //new is a keyword to create an instance of Department object using its constructor
-console.log(accounting); //Department {name: 'Accounting'}
+accounting.describe(); //executes describe function for Accounting Object
--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,9 @@ class  Department{
     constructor(n:string){//constructur to call it later and create instance of Department Object
         this.name=n; //Assigning n to name property;
     }
+    describe(this:Department){ //describe is a constructor function that refer to an object of type Department only.
+        console.log("Department Name:"+this.name);
+    }
 }
 const accounting=new Department("Accounting");//new is a keyword to create an instance of Department object using its constructor
-console.log(accounting); //Department {name: 'Accounting'}
+accounting.describe();//executes describe function for Accounting Object
