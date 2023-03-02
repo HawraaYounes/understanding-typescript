8b987c91b85ffc0c33eceb93ed2b4db27d6e917f
HawraaYounes <hawraayounes0@gmail.com>
2023-03-02 13:00:19 +0200

    Video 96: Working with Constraints

--- dist/app.js
+++ dist/app.js
@@ -5,3 +5,9 @@ function displayData(a) {
 }
 console.log(displayData("Hello World!")); //Hello World!
 console.log(displayData(12345)); //12345
+function getLength(a) {
+    return a.length;
+}
+console.log(getLength([2, 3, 7, 1, 9])); //5
+console.log(getLength("GUESS MY LENGTH!")); //16
+//console.log(getLength(1235)); //Error: numbers don't have '.length' property
--- src/app.ts
+++ src/app.ts
@@ -4,3 +4,14 @@
     }
     console.log(displayData<string>("Hello World!")) //Hello World!
     console.log(displayData<number>(12345)) //12345
+
+//Video 96: Working with Constraints
+    interface HasLength{
+      length:number
+    }
+    function getLength<T extends HasLength>(a:T){ //extends HasLength allow us to use .length property
+      return a.length; 
+    }
+    console.log(getLength([2,3,7,1,9])); //5
+    console.log(getLength("GUESS MY LENGTH!")); //16
+    //console.log(getLength(1235)); //Error: numbers don't have '.length' property
