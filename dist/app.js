9c2ac42be181b508605aecd81938f9cadf8678b2
HawraaYounes <hawraayounes0@gmail.com>
2023-03-02 05:28:21 +0200

    Video 83: Intersection Types

--- dist/app.js
+++ dist/app.js
@@ -1,1 +1,5 @@
 "use strict";
+function showType(args) {
+    console.log(args);
+}
+showType({ id: 1, left: "test", right: "test" }); // Output: {id: 1, left: "test", right: "test"}
---
+++ src/app.ts
@@ -0,0 +1,16 @@
+//Video 83: Intersection Types
+    type LeftType = {
+        id: number
+        left: string
+      } 
+      type RightType = {
+        id: number
+        right: string
+      }
+      
+      type IntersectionType = LeftType & RightType //createing a new type by combining leftType & rightType.
+      function showType(args: IntersectionType) {
+       console.log(args);
+      }
+
+      showType({ id: 1, left: "test", right: "test" }) // Output: {id: 1, left: "test", right: "test"}
