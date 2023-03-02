c4c477186dc1f07a85ef667a01f851f4ec8c771a
HawraaYounes <hawraayounes0@gmail.com>
2023-03-02 09:54:28 +0200

    Structuring Section 6 Files

---
+++ dist/section6/advanced-types.js
@@ -0,0 +1,101 @@
+"use strict";
+var _a;
+function showType(args) {
+    console.log(args);
+}
+showType({ id: 1, left: "test", right: "test" }); // Output: {id: 1, left: "test", right: "test"}
+function addNumbers(a, b) {
+    if (typeof a === 'number' && typeof b === 'number') {
+        return a + b;
+    }
+    if (typeof a === 'string' && typeof b === 'string') {
+        return a.concat(b);
+    }
+    throw new Error('Invalid arguments! Both arguments must be either numbers or strings.');
+}
+console.log(addNumbers('Hello ', 'World!')); //Hello World!
+console.log(addNumbers(10, 15)); //25
+//console.log(addNumbers('Hello ',25));//Error:Invalid arguments! Both arguments must be either numbers or strings.
+//'instanceof' type guard
+class Customer {
+    creditAllowed() {
+        return 'I am  a Customer.';
+    }
+}
+class Supplier {
+    inShortList() {
+        return 'I am a supplier.';
+    }
+}
+function signContract(partner) {
+    let message;
+    if (partner instanceof Customer) {
+        return message = partner.creditAllowed();
+    }
+    if (partner instanceof Supplier) {
+        return message = partner.inShortList();
+    }
+    throw new Error('Invalid argument! Argument should be Customer or Supplier');
+}
+const newPartner = new Supplier();
+console.log(signContract(newPartner));
+function printPaddingInformation(padding) {
+    if ('left' in padding) {
+        console.log('Padding to the Left: ' + padding.left);
+    }
+    if ('right' in padding) {
+        console.log('Padding to the Right: ' + padding.right);
+    }
+}
+printPaddingInformation({ id: 634, right: '70%' });
+function moveAnimal(animal) {
+    let speed;
+    switch (animal.type) { //checking the type of the animal
+        case 'bird':
+            speed = animal.flyingSpeed; //we can access flyingSpeed, since in this case we're sure that the animal type is bird.
+            break;
+        case 'horse':
+            speed = animal.runningSpeed; //we can access runningSpeed, since in this case we're sure that the animal type is horse.
+    }
+    console.log('Moving at speed: ' + speed);
+}
+moveAnimal({ type: 'bird', flyingSpeed: 10 }); //Moving at speed: 10
+//Video 86: Type Casting
+//const userInputElement = <HTMLInputElement>document.getElementById('user-input')!;
+const userInputElement = document.getElementById("user-input");
+if (userInputElement) {
+    userInputElement.value = 'Hi there!';
+    console.log(userInputElement.value);
+}
+//Video 87: Index Properties
+const salary1 = {
+    baseSalary: 100000,
+    yearlyBonus: 20000
+};
+const salary2 = {
+    contractSalary: 110000
+};
+function totalSalary(salaryObject) {
+    let total = 0;
+    for (const name in salaryObject) {
+        total += salaryObject[name];
+    }
+    return total;
+}
+console.log(totalSalary(salary1)); // 120000
+console.log(totalSalary(salary2)); // 110000
+//Video 89: Optional Chaining
+const adventurer = {
+    name: 'Alice',
+    cat: {
+        name: 'Bella'
+    }
+};
+const catName = (_a = adventurer.cat) === null || _a === void 0 ? void 0 : _a.name;
+console.log(catName); // Expected output: Bella
+//console.log(adventurer.dog?.name); // Error: Property 'dog' does not exist on type '{ name: string; cat: { name: string; }; 
+//Video 90: Nullish Coalescing
+const foo = null !== null && null !== void 0 ? null : 'DEFAULT'; //(??)logical operator that returns its right-hand side operand when its left-hand side operand is null or undefined, and otherwise returns its left-hand side operand.
+console.log(foo); // "DEFAULT"
+const number = 0 !== null && 0 !== void 0 ? 0 : 42;
+console.log(number); // 0
---
+++ src/section6/advanced-types.ts
@@ -0,0 +1,146 @@
+//Video 83: Intersection Types
+type LeftType = {
+    id: number
+    left: string
+  } 
+  type RightType = {
+    id: number
+    right: string
+  }
+  
+  type IntersectionType = LeftType & RightType //createing a new type by combining leftType & rightType.
+  function showType(args: IntersectionType) {
+   console.log(args);
+  }
+
+  showType({ id: 1, left: "test", right: "test" }) // Output: {id: 1, left: "test", right: "test"}
+
+//Video 84: More on Type Guards
+  //'typeof' type guard
+  type alphanumeric=string | number;
+  function addNumbers(a:string, b:string):string;//function overload (Video 88)
+  function addNumbers(a:number, b:number): number;//function overload
+  function addNumbers(a:alphanumeric,b:alphanumeric){
+    if(typeof a==='number' && typeof b==='number'){
+      return a+b;
+    }
+    if(typeof a==='string' && typeof b==='string'){
+      return a.concat(b);
+    }
+    throw new Error('Invalid arguments! Both arguments must be either numbers or strings.')
+  }
+  console.log(addNumbers('Hello ','World!')); //Hello World!
+  console.log(addNumbers(10,15)); //25
+  //console.log(addNumbers('Hello ',25));//Error:Invalid arguments! Both arguments must be either numbers or strings.
+
+   //'instanceof' type guard
+   class Customer {
+    creditAllowed(): string {
+        return 'I am  a Customer.';
+    }
+}
+
+class Supplier {
+    inShortList():string {
+        return 'I am a supplier.';
+    }
+}
+type BusinessPartner = Customer | Supplier;
+function signContract(partner: BusinessPartner) : string {
+    let message: string;
+    if (partner instanceof Customer) {
+        return message = partner.creditAllowed();
+    }
+
+    if (partner instanceof Supplier) {
+        return message = partner.inShortList();
+    }
+    throw new Error('Invalid argument! Argument should be Customer or Supplier')
+}
+const newPartner=new Supplier();
+console.log(signContract(newPartner));
+
+//'in' type gurard
+type Padding = LeftType | RightType;
+function printPaddingInformation(padding: Padding) {
+  if ('left' in padding) {
+    console.log('Padding to the Left: ' + padding.left);
+  }
+  if ('right' in padding) {
+    console.log('Padding to the Right: ' + padding.right);
+  }
+}
+printPaddingInformation({ id: 634, right: '70%' });
+
+//Video 85: Discriminated Unions
+interface Bird {
+  type: 'bird';
+  flyingSpeed: number;
+}
+
+interface Horse {
+  type: 'horse';
+  runningSpeed: number;
+}
+
+type Animal = Bird | Horse;
+
+function moveAnimal(animal: Animal) {
+  let speed;
+  switch (animal.type) { //checking the type of the animal
+    case 'bird':
+      speed = animal.flyingSpeed; //we can access flyingSpeed, since in this case we're sure that the animal type is bird.
+      break;
+    case 'horse':
+      speed = animal.runningSpeed; //we can access runningSpeed, since in this case we're sure that the animal type is horse.
+  }
+  console.log('Moving at speed: ' + speed);
+}
+
+moveAnimal({type: 'bird', flyingSpeed: 10}); //Moving at speed: 10
+
+//Video 86: Type Casting
+//const userInputElement = <HTMLInputElement>document.getElementById('user-input')!;
+const userInputElement = document.getElementById("user-input");
+if (userInputElement) {
+  (userInputElement as HTMLInputElement).value = 'Hi there!';
+  console.log((userInputElement as HTMLInputElement).value)
+}
+
+//Video 87: Index Properties
+const salary1 = {
+  baseSalary: 100_000,
+  yearlyBonus: 20_000
+};
+
+const salary2 = {
+  contractSalary: 110_000
+};
+function totalSalary(salaryObject: { [key: string]: number }) {//accepts arguments with unkown names and numbers, all what we know that thet're all of type 'number
+  let total = 0;
+  for (const name in salaryObject) {
+    total += salaryObject[name];
+  }
+  return total;
+}
+
+console.log(totalSalary(salary1)); // 120000
+console.log(totalSalary(salary2)) // 110000
+
+//Video 89: Optional Chaining
+const adventurer = {
+name: 'Alice',
+cat: {
+  name: 'Bella'
+}
+};
+
+const catName = adventurer.cat?.name;
+console.log(catName); // Expected output: Bella
+//console.log(adventurer.dog?.name); // Error: Property 'dog' does not exist on type '{ name: string; cat: { name: string; }; 
+
+//Video 90: Nullish Coalescing
+const foo = null ?? 'DEFAULT'; //(??)logical operator that returns its right-hand side operand when its left-hand side operand is null or undefined, and otherwise returns its left-hand side operand.
+console.log(foo); // "DEFAULT"
+const number = 0 ?? 42;
+console.log(number);// 0
