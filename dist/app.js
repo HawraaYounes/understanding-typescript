8d1deeda9c0e50d37400c91a606289c42f33193e
HawraaYounes <hawraayounes0@gmail.com>
2023-03-02 13:32:18 +0200

    Video 98: The keyof Constraint

--- dist/app.js
+++ dist/app.js
@@ -11,3 +11,10 @@ function getLength(a) {
 console.log(getLength([2, 3, 7, 1, 9])); //5
 console.log(getLength("GUESS MY LENGTH!")); //16
 //console.log(getLength(1235)); //Error: numbers don't have '.length' property
+//Video 98: The "keyof" Constraint
+function getProperty(obj, key) {
+    return obj[key];
+}
+let ob = { a: 1, b: 2, c: 3, d: 4 };
+console.log(getProperty(ob, "a")); //1
+//getProperty(ob, "m"); //Error:Argument of type '"m"' is not assignable to parameter of type '"a" | "b" | "c" | "d"'
--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,7 @@
 
 //Video 96: Working with Constraints
     interface HasLength{
-      length:number
+      length:number;
     }
     function getLength<T extends HasLength>(a:T){ //extends HasLength allow us to use .length property
       return a.length; 
@@ -15,3 +15,14 @@
     console.log(getLength([2,3,7,1,9])); //5
     console.log(getLength("GUESS MY LENGTH!")); //16
     //console.log(getLength(1235)); //Error: numbers don't have '.length' property
+
+//Video 98: The "keyof" Constraint
+    function getProperty<Type, Key extends keyof Type>(obj: Type, key: Key) {
+      return obj[key];
+    }
+    
+    let ob = { a: 1, b: 2, c: 3, d: 4 };
+    
+    console.log(getProperty(ob, "a"));//1
+    //getProperty(ob, "m"); //Error:Argument of type '"m"' is not assignable to parameter of type '"a" | "b" | "c" | "d"'
+
