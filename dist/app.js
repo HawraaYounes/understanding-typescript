f827dfb4b8b9ab79c7d0d8b32d6a21f45830c761
HawraaYounes <hawraayounes0@gmail.com>
2023-03-05 11:02:22 +0200

    Video 123: Interacting with DOM Elements

--- dist/app.js
+++ dist/app.js
@@ -3,9 +3,21 @@ class ProjectInput {
     constructor() {
         this.templateElement = document.getElementById('project-input');
         this.hostElement = document.getElementById('app');
-        const importedNode = document.importNode(this.templateElement.content, true); //creates a copy of form template and imports it to the current document. 
-        this.element = importedNode.firstElementChild;
+        const importedNode = document.importNode(this.templateElement.content, true); //creates a copy of template and imports it to the current document.
+        this.element = importedNode.firstElementChild; //returns form input from imported node
+        this.element.id = 'user-input'; //Add id to the form to apply the styling to it
+        this.titleInputElement = this.element.querySelector('#title'); //returns title input  
+        this.descriptionInputElement = this.element.querySelector('#description'); //returns descriptio input
+        this.peopleInputElement = this.element.querySelector('#people'); //returns people input
+        this.configure();
         this.attach();
+    }
+    handleSubmit(e) {
+        e.preventDefault();
+        console.log(this.titleInputElement.value);
+    }
+    configure() {
+        this.element.addEventListener('submit', this.handleSubmit.bind(this));
     }
     attach() {
         this.hostElement.insertAdjacentElement('afterbegin', this.element); //insert the copy of created template after begining of the div (between opening & closing tags)
--- src/app.ts
+++ src/app.ts
@@ -2,16 +2,32 @@ class ProjectInput {
     templateElement: HTMLTemplateElement;
     hostElement: HTMLDivElement;
     element: HTMLFormElement;
+    titleInputElement: HTMLInputElement;
+    descriptionInputElement: HTMLInputElement;
+    peopleInputElement: HTMLInputElement;
   
     constructor() {
       this.templateElement = document.getElementById('project-input')! as HTMLTemplateElement;
       this.hostElement = document.getElementById('app')! as HTMLDivElement;
   
-      const importedNode = document.importNode(this.templateElement.content,true);//creates a copy of form template and imports it to the current document. 
-      this.element = importedNode.firstElementChild as HTMLFormElement;
+      const importedNode = document.importNode(this.templateElement.content,true);//creates a copy of template and imports it to the current document.
+      this.element = importedNode.firstElementChild as HTMLFormElement;//returns form input from imported node
+      this.element.id='user-input';//Add id to the form to apply the styling to it
+
+      this.titleInputElement = this.element.querySelector('#title') as HTMLInputElement;//returns title input  
+      this.descriptionInputElement = this.element.querySelector('#description') as HTMLInputElement;//returns descriptio input
+      this.peopleInputElement = this.element.querySelector('#people') as HTMLInputElement;//returns people input
+  
+      this.configure();
       this.attach();
     }
-  
+    private handleSubmit(e:Event){
+        e.preventDefault();
+        console.log(this.titleInputElement.value)
+    }
+    private configure(){
+        this.element.addEventListener('submit',this.handleSubmit.bind(this));
+    }
     private attach() {
       this.hostElement.insertAdjacentElement('afterbegin', this.element);//insert the copy of created template after begining of the div (between opening & closing tags)
     }
