0013aac394bd383260d33026e17e1a992418ec79
HawraaYounes <hawraayounes0@gmail.com>
2023-03-05 16:32:37 +0200

    Video 125: Video 125: Fetching User Input

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,4 @@
+
 //Autobind Decorator
 function autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
     const originalMethod = descriptor.value;//save original method description
@@ -35,10 +36,38 @@ class ProjectInput {
       this.configure();
       this.attach();
     }
+    private gatherUserInput(): [string, string, number] | void {
+        const enteredTitle = this.titleInputElement.value;
+        const enteredDescription = this.descriptionInputElement.value;
+        const enteredPeople = this.peopleInputElement.value;
+    
+        if (
+          enteredTitle.trim().length === 0 ||
+          enteredDescription.trim().length === 0 ||
+          enteredPeople.trim().length === 0
+        ) {
+          alert('Invalid input, please try again!');
+          return;
+        } else {
+          return [enteredTitle, enteredDescription, +enteredPeople];
+        }
+      }
+    
+      private clearInputs() {
+        this.titleInputElement.value = '';
+        this.descriptionInputElement.value = '';
+        this.peopleInputElement.value = '';
+      }
     @autobind
     private handleSubmit(e:Event){
         e.preventDefault();
-        console.log(this.titleInputElement.value)
+        const userInput = this.gatherUserInput();
+        if (Array.isArray(userInput)) {
+          const [title, desc, people] = userInput;
+          console.log(title, desc, people);
+          this.clearInputs();
+        }
+    
     }
 
     private configure(){
