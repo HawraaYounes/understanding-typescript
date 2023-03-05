5506231efeff076558a2edc020b4c7b292047059
HawraaYounes <hawraayounes0@gmail.com>
2023-03-05 17:32:10 +0200

    Video 127: Rendering Project Lists

--- dist/app.js
+++ dist/app.js
@@ -42,7 +42,29 @@ function autobind(_, _2, descriptor) {
     };
     return adjDescriptor;
 }
-//class ProjrctInput
+// ProjectList Class
+class ProjectList {
+    constructor(type) {
+        this.type = type;
+        this.templateElement = document.getElementById('project-list');
+        this.hostElement = document.getElementById('app');
+        const importedNode = document.importNode(this.templateElement.content, true);
+        this.element = importedNode.firstElementChild;
+        this.element.id = `${this.type}-projects`;
+        this.attach();
+        this.renderContent();
+    }
+    renderContent() {
+        const listId = `${this.type}-projects-list`;
+        this.element.querySelector('ul').id = listId;
+        this.element.querySelector('h2').textContent =
+            this.type.toUpperCase() + ' PROJECTS';
+    }
+    attach() {
+        this.hostElement.insertAdjacentElement('beforeend', this.element);
+    }
+}
+//class ProjectInput
 class ProjectInput {
     constructor() {
         this.templateElement = document.getElementById('project-input');
@@ -110,3 +132,5 @@ __decorate([
     autobind
 ], ProjectInput.prototype, "handleSubmit", null);
 const prjInput = new ProjectInput(); //create an instance of the class ProjectInput
+const activePrjList = new ProjectList('active');
+const finishedPrjList = new ProjectList('finished');
--- src/app.ts
+++ src/app.ts
@@ -54,7 +54,42 @@ function autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
     return adjDescriptor;
   }
 
-//class ProjrctInput
+  // ProjectList Class
+class ProjectList {
+    templateElement: HTMLTemplateElement;
+    hostElement: HTMLDivElement;
+    element: HTMLElement;
+  
+    constructor(private type: 'active' | 'finished') {
+      this.templateElement = document.getElementById(
+        'project-list'
+      )! as HTMLTemplateElement;
+      this.hostElement = document.getElementById('app')! as HTMLDivElement;
+  
+      const importedNode = document.importNode(
+        this.templateElement.content,
+        true
+      );
+      this.element = importedNode.firstElementChild as HTMLElement;
+      this.element.id = `${this.type}-projects`;
+      this.attach();
+      this.renderContent();
+    }
+  
+    private renderContent() {
+      const listId = `${this.type}-projects-list`;
+      this.element.querySelector('ul')!.id = listId;
+      this.element.querySelector('h2')!.textContent =
+        this.type.toUpperCase() + ' PROJECTS';
+      
+    }
+  
+    private attach() {
+      this.hostElement.insertAdjacentElement('beforeend', this.element);
+    }
+  }
+  
+//class ProjectInput
 class ProjectInput {
     templateElement: HTMLTemplateElement;
     hostElement: HTMLDivElement;
@@ -138,6 +173,7 @@ class ProjectInput {
   }
   
   const prjInput = new ProjectInput(); //create an instance of the class ProjectInput
-
+  const activePrjList = new ProjectList('active');
+  const finishedPrjList = new ProjectList('finished');
   
   
