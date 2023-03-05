d554e1bb112463b3110d2c9f8c036bc706773ef3
HawraaYounes <hawraayounes0@gmail.com>
2023-03-05 16:44:30 +0200

    Video 126:  Creating a Re-Usable Validation Functionality-Update

--- src/app.ts
+++ src/app.ts
@@ -83,16 +83,33 @@ class ProjectInput {
         const enteredDescription = this.descriptionInputElement.value;
         const enteredPeople = this.peopleInputElement.value;
     
+        const titleValidatable: Validatable = {
+          value: enteredTitle,
+          required: true
+        };
+        const descriptionValidatable: Validatable = {
+          value: enteredDescription,
+          required: true,
+          minLength: 5
+        };
+        const peopleValidatable: Validatable = {
+          value: +enteredPeople,
+          required: true,
+          min: 1,
+          max: 5
+        };
+    
         if (
-          enteredTitle.trim().length === 0 ||
-          enteredDescription.trim().length === 0 ||
-          enteredPeople.trim().length === 0
+          !validate(titleValidatable) ||
+          !validate(descriptionValidatable) ||
+          !validate(peopleValidatable)
         ) {
           alert('Invalid input, please try again!');
           return;
         } else {
           return [enteredTitle, enteredDescription, +enteredPeople];
         }
+    
       }
     
       private clearInputs() {
