cb530cc557fc8e84668b3a95152e5a27a194565f
HawraaYounes <hawraayounes0@gmail.com>
2023-03-07 07:00:24 +0200

    Video 164: No Types Needed: class-transformer

--- dist/app.js
+++ dist/app.js
@@ -1,19 +1,14 @@
-//Video 162: Using JavaScript (!) Libraries with TypeScript
 import _ from 'lodash';
-console.log(_.shuffle([1,2,3]));
-
-//Video 164: No Types Needed: class-transformer
+console.log(_.shuffle([1, 2, 3]));
 import 'reflect-metadata';
 import { plainToClass } from 'class-transformer';
 import { Product } from './product.model';
-
 const products = [
-  { title: 'A Carpet', price: 29.99 },
-  { title: 'A Book', price: 10.99 }
+    { title: 'A Carpet', price: 29.99 },
+    { title: 'A Book', price: 10.99 }
 ];
-
 const loadedProducts = plainToClass(Product, products);
-
 for (const prod of loadedProducts) {
-  console.log(prod.getInformation());
+    console.log(prod.getInformation());
 }
+//# sourceMappingURL=app.js.map
